671bc1798923555adbd4b6901c2cbd4c331e758e
Eduardo Fuentes <eduardo.fuentesreyes@gmail.com>
2023-11-09 16:36:08 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  host: "smpt.hostinger.com",
+  secure: true,
+  secureConnection: false,
+  tls: {
+    ciphers: "SSLv3",
+  },
+  requireTLS: true,
+  port: 465,
+  debug: true,
+  connectionTimeout: 10000,
+  auth: {
+    user: process.env.HOSTINGER_USER,
+    pass: process.env.HOSTINGER_PASS,
+  },
+});
+
+app.post("/send-email", (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+	// Create email content
+  const mailOptions = {
+    from: process.env.MAIL_SENDER,
+    to: email,
+    subject: subject,
+    text: `${message}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
